261edbca595d5c65db8f15da2aa708760729efda
marq <classicvector63@gmail.com>
2025-02-09 14:01:02 +0530

    update setting templates

--- maxlevel/maxlevel/views.py
+++ maxlevel/maxlevel/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Hi there, This is my first django web")
+def home(req):
+    # return HttpResponse("Hi there, This is my first django web")
+    return render(req,'index.html')
 
 def about(request):
     return HttpResponse("Hi there, This is my first django web")
